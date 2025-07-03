00d62569bb2a06c2c0ea5cbcdf093e68fa4ad10b
Felipe Vogel <fps.vogel@gmail.com>
2025-07-03 18:33:43 -0400

    Improve 2024#02

--- src/2024/02.rb
+++ src/2024/02.rb
@@ -2,55 +2,25 @@
 module Year2024
   class Day02
     def part_1(input_file)
-      rows = input_file
+      input_file
         .readlines(chomp: true)
         .map { |line| line.split.map(&:to_i) }
-
-      good_row_count(rows, tolerate_one_bad_column: false)
+        .count { |row|
+          row.each_cons(2).all? { |col_a, col_b| col_b.between?(col_a + 1, col_a + 3) } ||
+            row.each_cons(2).all? { |col_a, col_b| col_b.between?(col_a - 3, col_a - 1) }
+        }
     end
 
     def part_2(input_file)
-      rows = input_file
+      input_file
         .readlines(chomp: true)
         .map { |line| line.split.map(&:to_i) }
-
-      good_row_count(rows, tolerate_one_bad_column: true)
-    end
-
-    private
-
-    def good_row_count(rows, tolerate_one_bad_column:)
-      good_rows = []
-
-      rows.each do |row|
-        if row_good?(row)
-          good_rows << row
-        elsif tolerate_one_bad_column
-          row.each_index do |index|
-            row_without_element = row.dup.tap { it.delete_at(index) }
-            if row_good?(row_without_element)
-              good_rows << row_without_element
-              break
-            end
-          end
-        end
-      end
-
-      good_rows.count
-    end
-
-    def row_good?(row)
-      sorted_row = row.sort
-
-      if row == sorted_row || row == sorted_row.reverse
-        absolute_values = row.each_cons(2).map { (_1 - _2).abs }
-
-        if absolute_values.all? { (1..3).cover? it }
-          return true
-        end
-      end
-
-      false
+        .count { |complete_row|
+          complete_row.combination(complete_row.size - 1).any? { |row|
+            row.each_cons(2).all? { |col_a, col_b| col_b.between?(col_a + 1, col_a + 3) } ||
+              row.each_cons(2).all? { |col_a, col_b| col_a.between?(col_b + 1, col_b + 3) }
+          }
+        }
     end
   end
 end
