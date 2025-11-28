b704fc129327a22da1d02b73da5fd597cb1760b0
Felipe Vogel <fps.vogel@gmail.com>
2025-11-27 21:30:04 -0500

    Improve 2016#01

--- src/2016/01.rb
+++ src/2016/01.rb
@@ -1,57 +1,47 @@
 # https://adventofcode.com/2016/day/1
+
+require "matrix"
+
 module Year2016
   class Day01
+    DIRS = [
+      Vector[0, 1],
+      Vector[1, 0],
+      Vector[0, -1],
+      Vector[-1, 0]
+    ]
+
     def part_1(input_file)
-      moves = input_file.read.split(", ")
-
-      pos = [0, 0]
-      dir = [0, 1]
-
-      moves.each do |move|
-        turn = move[0]
-        distance = move[1..].to_i
-
-        dir =
-          if turn == "R"
-            [dir[1], -dir[0]]
-          else
-            [-dir[1], dir[0]]
-          end
-
-        pos[0] += dir[0] * distance
-        pos[1] += dir[1] * distance
-      end
-
-      pos.map(&:abs).sum
+      final_position = follow_directions(input_file)
+      final_position.sum(&:abs)
     end
 
     def part_2(input_file)
-      moves = input_file.read.split(", ")
+      follow_directions(input_file) do |past_positions, new_position|
+        if past_positions.include?(new_position)
+          return new_position.sum(&:abs)
+        end
+      end
+    end
 
-      positions = [[0, 0]]
-      dir = [0, 1]
+    private
 
-      moves.each do |move|
-        turn = move[0]
-        distance = move[1..].to_i
+    def follow_directions(input_file)
+      positions = [Vector[0, 0]]
+      direction = 0
 
-        dir =
-          if turn == "R"
-            [dir[1], -dir[0]]
-          else
-            [-dir[1], dir[0]]
-          end
+      input_file.read.scan(/(\w)(\d+)/).each do |turn, distance|
+        direction += (turn == "R") ? 1 : -1
+        direction %= DIRS.size
 
-        distance.times do
-          new_pos = [positions.last[0] + dir[0], positions.last[1] + dir[1]]
-
-          if positions.include?(new_pos)
-            return new_pos.map(&:abs).sum
-          end
-
+        distance.to_i.times do
+          new_pos = positions.last + DIRS[direction]
+          yield positions, new_pos if block_given?
           positions << new_pos
         end
       end
+
+      positions.last
     end
   end
 end
