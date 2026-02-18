786f23d7cd1448239b0f25762bc457a1736b3775
Felipe Vogel <fps.vogel@gmail.com>
2026-02-18 09:19:12 -0500

    Improve 2017#02

--- src/2017/02.rb
+++ src/2017/02.rb
@@ -2,20 +2,13 @@
 module Year2017
   class Day02
     def part_1(input_file)
-      input_file
-        .readlines
-        .sum { it.split.map(&:to_i).minmax.reduce(:-).abs }
+      rows = input_file.map { it.split.map(&:to_i) }
+      rows.sum { it.minmax.reduce(:-).abs }
     end
 
     def part_2(input_file)
-      rows = input_file.readlines(chomp: true).map { it.split.map(&:to_i) }
-
-      rows.sum { |row|
-        row
-          .combination(2)
-          .find { |a, b| a % b == 0 || b % a == 0 }
-          .then { |a, b| [a, b].max / [a, b].min }
-      }
+      rows = input_file.map { it.split.map(&:to_i) }
+      rows.sum { it.permutation(2).find { |a, b| a % b == 0 }.reduce(:/) }
     end
   end
 end
